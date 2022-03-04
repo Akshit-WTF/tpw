341f899e14a0161ba9455e6a7042c8469ae0a643
Akshit Kumar <akshitk40@gmail.com>
2022-03-05 04:37:44 +0530

    Removed Copy Pasta

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,7 @@ module.exports = defineConfig({
     build: {
         rollupOptions: {
             input: {
-                index: resolve(__dirname, './src/index.html'),
-                about: resolve(__dirname, './src/about.html'),
-                domain_info: resolve(__dirname, './src/domain-info.html'),
-                docs: resolve(__dirname, './src/docs/index.html'),
-                whois: resolve(__dirname, './src/whois.html'),
-                adc: resolve(__dirname, './src/adc_report.html'),
-                supporters: resolve(__dirname, './src/supporters.html'),
-                "404": resolve(__dirname, './src/404.html'),
+                index: resolve(__dirname, './src/index.html')
             }
         }
     },
